ae3b4adfadc81c425f76ccb3acbd39a59643bafd
nekobu <nekobu.crewless184@passinbox.com>
2025-09-17 11:01:33 -0400

    Another push to try and actually send back a response

--- api/flavor-text.js
+++ api/flavor-text.js
@@ -4,6 +4,7 @@ export default function handler(req, res) {
     // If the request is a GET one, send back a random flavor text
     if (req.method === 'GET') {
         const ft = randFT();
+        res.status(200).json(ft);
     } else { // Otherwise, send back a 405; this is only taking GET requests.
         res.setHeader('Allow', ['GET']);
         res.status(405).end('Method ${req.method} Not Allowed');
