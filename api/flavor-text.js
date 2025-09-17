34c6b09471bd97de9bdb5f86312ebe403599b926
nekobu <nekobu.crewless184@passinbox.com>
2025-09-17 11:10:52 -0400

    Change single quotes to backticks

--- api/flavor-text.js
+++ api/flavor-text.js
@@ -7,7 +7,7 @@ export default function handler(req, res) {
         res.status(200).json(text);
     } else { // Otherwise, send back a 405; this is only taking GET requests.
         res.setHeader('Allow', ['GET']);
-        res.status(405).end('Method ${req.method} Not Allowed');
+        res.status(405).end(`Method ${req.method} Not Allowed`);
     }
 }
 
