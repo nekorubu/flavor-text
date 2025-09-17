f651c1a7a7fb1f80e8400c18aa23e63a797516e1
nekobu <nekobu.crewless184@passinbox.com>
2025-09-16 21:42:55 -0400

    Slight refactor to try and use Vercel

---
+++ api/flavor-text.js
@@ -0,0 +1,17 @@
+const app = require('express')();
+const ft = require('./data/text.json');
+
+export default function handler(req, res) {
+    // If the request is a GET one, send back a random flavor text
+    if (req.method === 'GET') {
+        const ft = randFT();
+    } else { // Otherwise, send back a 405; this is only taking GET requests.
+        res.setHeader('Allow', ['GET']);
+        res.status(405).end('Method ${req.method} Not Allowed');
+    }
+}
+
+// Picks the flavor text at the requested index, or if left blank, a random one
+function randFT(n = Math.floor(Math.random() * ft.length)) {
+    return ft[n];
+}
