6c905e6d01ca646219a4314705db7ba521410819
nekobu <nekobu.crewless184@passinbox.com>
2025-09-17 11:36:30 -0400

    Change some variable names and add AI disclosure

--- api/flavor-text.js
+++ api/flavor-text.js
@@ -1,10 +1,11 @@
+// This code was mainly written by Claude. I used it to try and get the API up and running, then changed a few variables once it was finally running.
 const ft = require('../data/text.json');
 
 export default function handler(req, res) {
     // If the request is a GET one, send back a random flavor text
     if (req.method === 'GET') {
-        const randomFlavor = randFT(); // Get random flavor text
-        res.status(200).json(randomFlavor);
+        const flavor = randFT(); // Get random flavor text
+        res.status(200).json(flavor);
     } else { // Otherwise, send back a 405; this is only taking GET requests.
         res.setHeader('Allow', ['GET']);
         res.status(405).end(`Method ${req.method} Not Allowed`);
